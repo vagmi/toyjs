668cbe1ba4802e76a29ae2f787154359bc977ff8
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2026-02-25 15:14:53 -0500

    Add a timeout to the mix.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 import { sum } from './math.js';
 
 print("Hello from JS Module!");
-print("10 + 20 = " + sum(10, 20));
+setTimeout(() => {
+    print("Let us add!");
+    print("10 + 20 = " + sum(10, 20));
+}, 3000);
