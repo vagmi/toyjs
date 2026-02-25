f12e60b3ae40ceb3e40d4a4b373df2252dd1566b
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2026-02-25 15:16:51 -0500

    Add fetch and top level async await example.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 import { sum } from './math.js';
 
 print("Hello from JS Module!");
+
+let resp = await fetch('https://ipinfo.io/json');
+let content = await resp.json();
+print(JSON.stringify(content, null, 2));
+
 setTimeout(() => {
     print("Let us add!");
     print("10 + 20 = " + sum(10, 20));
